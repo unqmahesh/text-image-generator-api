01d725ebcc0f2659184cdea98c3560bd69d614e8
mahesh <mahesh@gmail.com>
2024-03-05 20:14:48 +0530

    added api health check opt

--- router/index-router.js
+++ router/index-router.js
@@ -6,5 +6,6 @@ import {validators, validation} from '../middlewares/s-d-Validation.js'
 const indexRouter = express.Router()
 
 indexRouter.route('/s-d').post(validators, validation, sdImgGen)
+indexRouter.route('/health').get((req, res)=>{res.status(200).send('This api is healthy')})
 
 export default indexRouter
